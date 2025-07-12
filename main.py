0c8a9e322e658a8b8e62b250fca7ed43c6c415eb
Geetha Kondapalli <geethakondapalli@gmail.com>
2025-07-12 21:09:36 +0100

    cors

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",  # React development server
         "http://127.0.0.1:3000",
-        "https://distinguished-art.railway.internal"
+        "https://distinguished-art.railway.internal",
         "https://frontend-production-6ab1.up.railway.app"  # Your production domain
     ],  # Configure this properly for production
     allow_credentials=True,
